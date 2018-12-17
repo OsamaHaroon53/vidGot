24097548fce4a7516d5ccb8f1a0c850009887c82
OsamaHaroon53 <sonijaosama@gmail.com>
2018-12-17 22:23:39 +0500

    update 'Add suggestion' and complete 'Edit Video suggestion'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 
 const app = express();
 const port = 3000;
@@ -9,12 +10,15 @@ require('./connection');
 const course = require('./models/CourseIdea');
 
 //body-parser
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+//method override for put and delete
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -29,41 +33,47 @@ app.get('/course/add', (req, res) => {
 });
 
 app.get('/course/edit/:id', (req, res) => {
-    res.render('course/edit');
+    course.findOne({ _id: req.params.id })
+        .then(data => {
+            res.render('course/edit', { course: data });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 app.get('/course', (req, res) => {
     course.find()
-        .sort({date: 'desc'})
-        .then(data=>{
-            res.render('course/index',{data: data});
+        .sort({ date: 'desc' })
+        .then(data => {
+            res.render('course/index', { data: data });
         })
-        .catch(err=>{
+        .catch(err => {
             console.log(err);
         });
-    
+
 });
 
 app.post('/course', (req, res) => {
     let errors = [];
-    if(!req.body.name){
-        errors.push({text: 'Empty Course Name'});
+    if (!req.body.name) {
+        errors.push({ text: 'Empty Course Name' });
     }
-    if(!req.body.description){
-        errors.push({text: 'Empty Course Name'});
+    if (!req.body.description) {
+        errors.push({ text: 'Empty description' });
     }
-    if(!req.body.reason){
-        errors.push({text: 'Empty Course Name'});
+    if (!req.body.reason) {
+        errors.push({ text: 'Explain why this course?' });
     }
-    if(errors.length > 0){
-        res.render('course/add',{
+    if (errors.length > 0) {
+        res.render('course/add', {
             errors: errors,
             name: req.body.name,
             description: req.body.description,
             reason: req.body.reason
         });
     }
-    else{
+    else {
         let data = {
             CourseName: req.body.name,
             Description: req.body.description,
@@ -71,14 +81,53 @@ app.post('/course', (req, res) => {
         }
         new course(data)
             .save()
-            .then(course=>{
+            .then(course => {
                 res.redirect('/course');
             })
-            .catch(err=>{
+            .catch(err => {
                 console.log(err);
             });
     }
-})
+});
+
+app.put('/course', (req, res) => {
+    let errors = [];
+    if (!req.body.name) {
+        errors.push({ text: 'Empty Course Name' });
+    }
+    if (!req.body.description) {
+        errors.push({ text: 'Empty description' });
+    }
+    if (!req.body.reason) {
+        errors.push({ text: 'Explain why this course?' });
+    }
+    if (errors.length > 0) {
+        res.render('course/edit', {
+            errors: errors,
+            course: {
+                CourseName: req.body.name,
+                Description: req.body.description,
+                Reason: req.body.reason,
+                _id: req.body._id
+            }
+        });
+    }
+    else {
+        course.findById(req.body._id)
+            .then(newCourse => {
+                newCourse.CourseName = req.body.name;
+                newCourse.Description = req.body.description;
+                newCourse.Reason = req.body.reason;
+                newCourse.save()
+                    .then(course => {
+                        res.redirect('/course');
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+});
 
 app.listen(port, () => {
     console.log(`App listen on port ${port}`);
